614235ce28936c353d1ab6b5818729a0fea043fe
Aswin Ashok <aswinashok3907@gmail.com>
2025-06-13 00:13:42 +0530

    After using ssh keys

--- code/Basics.py
+++ code/Basics.py
@@ -1,5 +1,5 @@
 original_dict = {'a': 1, 'b': 2, 'c': 3}
 copied_dict = original_dict.copy()
 
-copied_dict['a'] = 12222500
+copied_dict['a'] = 10
 print(original_dict,copied_dict,sep="hello",end="*//") 
