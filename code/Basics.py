79bfb58879f0538be4e7a600dfca350055c54ab1
ASWIN ASHOK <aswinashok3907@gmail.com>
2025-06-16 16:00:24 +0530

    Delete code directory

--- code/Basics.py
+++
@@ -1,9 +0,0 @@
-original_dict = {'a': 1, 'b': 2, 'c': 3}
-copied_dict = original_dict.copy()
-
-copied_dict['a'] = 20
-print(original_dict,copied_dict,sep="hello",end="*//") 
-#bug fix
-#feature 1
-#commit1
-#commit2
