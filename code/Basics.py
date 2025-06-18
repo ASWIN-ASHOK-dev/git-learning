ce2e0c738bf5b5d0ac44334d041704c3a3c623bd
Aswin Ashok Linux <aswinashokstudy@gmail.com>
2025-06-19 00:19:10 +0530

    confirming color

--- code/Basics.py
+++ code/Basics.py
@@ -1,7 +1,7 @@
 original_dict = {'a': 1, 'b': 2, 'c': 3}
 copied_dict = original_dict.copy()
 
-copied_dict['a'] = 20
+copied_dict['a'] = 30
 print(original_dict,copied_dict,sep="hello",end="*//") 
 #bug fix
 #feature 1
